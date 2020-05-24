35751f7ea48a6ebc60fddf66892d42688642225e
Hydra <ryanandrews135@gmail.com>
2020-05-24 16:26:14 -0600

    Push

---
+++ ElvUI_PVPDT/ElvUI_PVPDT.lua
@@ -0,0 +1,53 @@
+local E = unpack(ElvUI)
+local DT = E:GetModule("DataTexts")
+
+local BreakUpLargeNumbers = BreakUpLargeNumbers
+local GetPVPLifetimeStats = GetPVPLifetimeStats
+local Label = KILLS
+local String = "%s: %s"
+local Panel
+
+local OnEvent = function(self, event, unit)
+	if (unit and unit ~= "player") then
+		return
+	end
+	
+	self.text:SetFormattedText(String, Label, BreakUpLargeNumbers(GetPVPLifetimeStats()))
+	
+	Panel = self
+end
+
+local OnClick = function()
+	ToggleCharacter("HonorFrame")
+end
+
+local OnEnter = function(self)
+	DT:SetupTooltip(self)
+	
+	local Honorable, Dishonorable = GetPVPLifetimeStats()
+	local Rank = UnitPVPRank("player")
+	
+	if (Rank > 0) then
+		local Name, Number = GetPVPRankInfo(Rank, "player")
+		
+		DT.tooltip:AddDoubleLine(Name, format("%s %s", RANK, Number))
+		DT.tooltip:AddLine(" ")
+	end
+	
+	DT.tooltip:AddDoubleLine(HONORABLE_KILLS, BreakUpLargeNumbers(Honorable), 1, 1, 1, 1, 1, 1)
+	DT.tooltip:AddDoubleLine(DISHONORABLE_KILLS, BreakUpLargeNumbers(Dishonorable), 1, 1, 1, 1, 1, 1)
+	
+	DT.tooltip:Show()
+end
+
+local ValueColorUpdate = function(hex)
+	String = strjoin("", "%s: ", hex, "%s|r")
+	
+	if (Panel ~= nil) then
+		OnEvent(Panel)
+	end
+end
+
+E.valueColorUpdateFuncs[ValueColorUpdate] = true
+
+DT:RegisterDatatext("PVP", {"PLAYER_PVP_KILLS_CHANGED", "PLAYER_ENTERING_WORLD"}, OnEvent, nil, OnClick, OnEnter, nil, PVP)
